f592d092d033ad5a5a594a92f324545795a34cb5
cerenarslan1 <cerenarrsllan@gmail.com>
2024-03-20 14:30:30 +0300

    AdonetCon

---
+++ Adonet_Baglanti/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Adonet_Baglanti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.buttonKaydet = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SOYAD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 212);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 226);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.AD,
+            this.SOYAD});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 215);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 223);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(41, 46);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(27, 16);
+            this.lblAd.TabIndex = 1;
+            this.lblAd.Text = "Ad:";
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Location = new System.Drawing.Point(41, 103);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(50, 16);
+            this.lblSoyad.TabIndex = 2;
+            this.lblSoyad.Text = "Soyad:";
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(109, 46);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(144, 22);
+            this.TxtAd.TabIndex = 3;
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Location = new System.Drawing.Point(109, 103);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(144, 22);
+            this.TxtSoyad.TabIndex = 4;
+            // 
+            // buttonKaydet
+            // 
+            this.buttonKaydet.Location = new System.Drawing.Point(308, 73);
+            this.buttonKaydet.Name = "buttonKaydet";
+            this.buttonKaydet.Size = new System.Drawing.Size(89, 24);
+            this.buttonKaydet.TabIndex = 5;
+            this.buttonKaydet.Text = "Kaydet";
+            this.buttonKaydet.UseVisualStyleBackColor = true;
+            this.buttonKaydet.Click += new System.EventHandler(this.buttonKaydet_Click);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.Width = 125;
+            // 
+            // AD
+            // 
+            this.AD.HeaderText = "AD";
+            this.AD.MinimumWidth = 6;
+            this.AD.Name = "AD";
+            this.AD.Width = 125;
+            // 
+            // SOYAD
+            // 
+            this.SOYAD.HeaderText = "SOYAD";
+            this.SOYAD.MinimumWidth = 6;
+            this.SOYAD.Name = "SOYAD";
+            this.SOYAD.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonKaydet);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.lblSoyad);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.Button buttonKaydet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SOYAD;
+    }
+}
+
---
+++ Adonet_Baglanti/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adonet_Baglanti
+{
+    public partial class Form1 : Form
+    {
+        //SqlConnection _baglanti = new SqlConnection("Data Source=.; Initial Catalog= Northwind;Persist Security Info=False; UserID=sa; Password=1234");
+        SqlConnection _baglanti = new SqlConnection("Server=Localhost;Database=Northwind;trusted_Connection=True");
+        public Form1()
+        {
+            InitializeComponent();
+            PersonelListele();
+        }
+        void PersonelListele()
+        {
+         dataGridView1.Rows.Clear();
+         _baglanti.Open();
+            SqlCommand cmd = new SqlCommand("select PersonelId,Adi,Soyadi from Personeller", _baglanti);
+            SqlDataReader dr= cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                dataGridView1.Rows.Add(dr[0], dr[1], dr[2]);
+            }
+            _baglanti.Close();
+        }
+
+        private void buttonKaydet_Click(object sender, EventArgs e)
+        {
+            //INSERT Personeller ( AD, Soyad) VALUES('Ceren','Arslan')
+            _baglanti.Open();
+            SqlCommand cmd = new SqlCommand("INSERT Personeller ( Adi, Soyadi) VALUES(@p1,@p2)", _baglanti);
+            cmd.Parameters.AddWithValue("@p1",TxtAd.Text);
+            cmd.Parameters.AddWithValue("@p2", TxtSoyad.Text);
+            cmd.ExecuteNonQuery();
+            _baglanti.Close();
+            PersonelListele();
+            TxtAd.Text = "";
+            TxtSoyad.Text = "";
+        }
+    }
+}
