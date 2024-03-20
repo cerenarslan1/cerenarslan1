ff4316ed859e1c9053d382220b15bb836e2e161c
cerenarslan1 <cerenarrsllan@gmail.com>
2024-03-21 01:07:59 +0300

    switchcase

---
+++ SWITCH 01.1123/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SWITCH_01._1123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Switch kullanarak mevsimleri yazdıralım.
+            //Kullanıcı mevsimi girsin örneğin =yaz çıktı olarak: Haziran Temmuz Ağustos.
+
+            Console.WriteLine("Lütfen Mevsimi Giriniz.");
+            string mevsim = Console.ReadLine();
+
+            switch (mevsim)
+            {
+                case "ilkbahar":
+                    Console.WriteLine("Mart, Nisan,Mayıs");
+                    break;
+                case "yaz":
+                    Console.WriteLine("Haziran, Temmuz, Ağustos");
+                    break;
+                case "sonbahar":
+                    Console.WriteLine("Eylül, Ekim, Kasım");
+                    break;
+                case "kış":
+                    Console.WriteLine("Aralık, Ocak, Şubat");
+                    break;
+                default:
+                    Console.WriteLine("Böyle bir mevsim yoktur");
+                    break;
+
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
