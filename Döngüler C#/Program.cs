a923c1373d2879abb39c144fd6e4e4c00b0ee7af
cerenarslan1 <cerenarrsllan@gmail.com>
2024-03-21 01:11:15 +0300

    donguler

---
+++ Döngüler C#/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Döngüler_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //For Döngüsü
+            //a=> Genellikle başlangıç değeri ve ismi verilen değişken burada tanımlanır. Ardışık algoritmalarda 
+            //başlangıç değerine ihtiyaç vardır ve burada tanımlayabiliriz. Bu tanımlamanın burada olması zorunlu değildir.
+
+            //b=> Herhangi bir koşul ifadesi tanımlanabilir. Şart sağlandığı sürece döngü çalışmaya devam edecektir.
+
+            //c=> Genellikle başlan değerini arttırmak ya da azaltmak için burası kullanılır. Kullanımı zorunlu değildir.
+
+
+
+            //for ( a ; b ; c )
+            //{
+            //Koşul true olduğu sürece bu scope alanı tetiklenecek ve döngü çalışacaktır.
+
+            //Console.WriteLine("--------------");
+            //int[] sayılar = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+            //for (int j = 0; j < sayılar.Length; j++)
+            //{
+            //    Console.WriteLine(sayılar[j]);
+
+
+            //}
+
+            //Console.ReadKey();
+
+
+
+            //int[] tekSayılar = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+
+            //for (int j = 0; j < tekSayılar.Length; j++)
+            //{
+            //    if (tekSayılar[j] % 2 == 1)
+            //    {
+            //        Console.WriteLine(tekSayılar[j]);
+            //    }
+
+            //}
+            //Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
