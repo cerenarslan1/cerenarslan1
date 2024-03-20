70c283dcbfaa11e22a48cd1781b1fdf44854ae05
cerenarslan1 <cerenarrsllan@gmail.com>
2024-03-21 00:56:51 +0300

    operatorler

---
+++ 24.10.23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _24._10._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //OPERATÖRLER
+            int sayı1 = 5;
+            int sayı2 = 18;
+            int toplam = sayı1 + sayı2;
+            int fark = sayı2 - sayı1;
+            int bolum = sayı2 / sayı1;
+            float s1 = 10;
+            float s2 = 4;
+            float sonuc = s1 / s2;
+            Console.WriteLine(toplam);
+            Console.WriteLine(fark);
+            Console.WriteLine(bolum);
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+
+
+        }
+    }
+}
