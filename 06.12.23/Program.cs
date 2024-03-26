a89154f15ca7a613924a32eeb5a136b26e73630e
cerenarslan1 <cerenarrsllan@gmail.com>
2024-03-26 17:04:20 +0300

    06.12.23

---
+++ 06.12.23/Program.cs
@@ -0,0 +1,126 @@
+﻿namespace _06._12._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //0 ila 100 arasında rastgele olarak verilen 10 sayıdan tek ve çift olanların sayısını bulan program yazınız.
+
+
+            int[] sayilar = new int[10];
+            int cift = 0, tek = 0;
+
+            Random random = new Random();
+            random.Next(1, 100);
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                if (sayilar[i] % 2 == 0)
+                {
+                    cift++;
+                    continue;
+                }
+                else
+                {
+                    tek++;
+                }
+
+            }
+            Console.WriteLine($"çift sayıların adeti:{cift} tek sayıların adeti:, {tek}");
+            Console.ReadLine();
+
+
+            ////bir alışveriş mağazasında 100 liraya kadar alışverişe yüzde 20, 100-200 arası alışverişlere yüzde 30, 200 liradan fazla alışverişlere yüzde 40 indirim yapılmaktadır. Girilen alışveriş fiyatına göre indirimli fiyatı bulan program yazınız.
+            ////Console.WriteLine("kaç liralık alışveriş yaptınız?");
+            Console.WriteLine("Alışveriş yapılacak tutarı giriniz");
+            double fiyat =Convert.ToInt32(Console.ReadLine());
+            double yuzde = 0;
+            double sonfiyat = 0;
+            if (fiyat <= 100)
+           {
+              sonfiyat = (fiyat * 0.8);
+               Console.WriteLine("100 liraya kadar olan alışverişlerinizde fiyat yüzde 20 düşüyor. Fiyat:"+sonfiyat);
+
+           }
+           else if (fiyat > 100 && fiyat < 200)
+           {
+           sonfiyat = (fiyat * 0.3);
+           Console.WriteLine("100-200 liralık alışverişlerinizde fiyat yüzde 30 düşüyor. Fiyat:"+sonfiyat );
+            }   
+            else if(fiyat > 200)
+            {
+                sonfiyat = (fiyat * 0.4);
+                Console.WriteLine("200 liradan fazla olan alışverişlerde fiyat yüzde 40 düşüyor.Fiyat:"+sonfiyat);
+            }
+            Console.ReadLine();
+
+
+            //elemanları rastgele verilen bir dizideki en büyük ve en küçük elemanı ekrana yazdırınız. Dizinin alabileceği eleman sayısını kullanıcı belirlesin 
+            //Console.WriteLine("lütfen sayı giriniz");
+            //int elemanlar = Convert.ToInt32(Console.ReadLine());
+            //int[]dizi=new int[elemanlar]; 
+
+            for (int i = 0; i < elemanlar; i++)
+            {
+                Console.WriteLine("{0}. sayı indexini giriniz:", (i + 1));
+                dizi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(dizi);
+            foreach (int i in dizi)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+
+            //kullanıcıya kaç tane sayıyı toplayacağını sorup, kullanıcının toplayacağı kadar sayıyı kullanıcıya TEK TEK SORAN VE KULLANICININ yazdığı bu sayıların toplamını ekrana yazan uygulama yazınız.
+            Console.WriteLine("Kaç adet sayı girmek istersiniz");
+            int adet = int.Parse(Console.ReadLine());
+            int toplam = 0;
+            for (int i = 0; i < adet; i++)
+            {
+                Console.WriteLine("{0}. sayıyı giriniz",(i+1));
+                toplam += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("girilen sayıların toplamı:"+toplam);
+
+
+            //bir dizi oluşturun içine 20 tane rastgele eleman alsın. Daha sonra bu listenin ortalamasını alın . Her bir dizi elemanı için ortalamadan büyük..
+            int[] sayilar = new int[20];
+            Random rnd = new Random();
+            int buyuk = 0;
+            int kucuk = 0;
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i] = rnd.Next(300);
+            }
+            double ortalama =sayilar.Average();
+           for (int i = 0; i < sayilar.Length; i++)
+            {
+                if (sayilar[i] < ortalama)
+                {
+                    kucuk++;
+                }
+                if (sayilar[i] > ortalama)
+                {
+                    buyuk++;
+                }
+            }
+           Console.WriteLine($"Dizinin ortalamasından büyük olan değerlerin adedi {buyuk} küçük olanların adedi {kucuk}");
+
+
+
+
+        }
+
+    }
+
+
+}
+
+
+        
+
+            
+  
---
+++ tekrar çalışmam 07.12.23/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace tekrar_çalışmam_07._12._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kosullar
+
+            //Bir üniversitede öğrencilerin ortalamaları 50 ve üzerinde ise dersten başarılı sayılmaktadırlar. Öğrencinin vize notu ortalamaya % 40, final notu % 60 etki ettiği bildiğine göre öğrencilerin ders başarı durumlarını hesaplayan programı yapalım.
+
+            int vize;
+            int final;
+            double ortalama;
+            Console.WriteLine("lütfen vize notunuzu giriniz");
+            vize = int.Parse(Console.ReadLine());
+            Console.WriteLine("lütfen final notunuzu giriniz");
+            final = int.Parse(Console.ReadLine());
+            ortalama = (vize * 40 / 100) + (final * 60 / 100);
+            if (ortalama >= 0)
+            {
+                Console.WriteLine("tebrikler,ortalamanız {0} dersten geçtiniz.", ortalama);
+            }
+            else
+            {
+                Console.WriteLine("ortalamanız {0} dersten kaldınız.", ortalama);
+            }
+            Console.ReadLine();
+
+
+
+
+            //DİZİLER
+
+            //Eleman sayısını kullanıcının belirlediği bir dizi oluşturarak içine kullanıcının eleman girmesini sağlayın.
+            //int mevcut;
+            //Console.WriteLine("sınıf mevcutu giriniz.");
+            //mevcut = int.Parse(Console.ReadLine());
+            //string[] ogrenci = new string[mevcut];
+            //for (int i = 0; mevcut > 0; i++)
+            //{
+            //    Console.WriteLine("lütfen öğrenci adı giriniz.");
+            //    ogrenci[i] = Console.ReadLine();
+            //}
+            //Console.ReadLine();    
+
+
+            //20 elemanlı diziye rasgele -100 ile +100 arası sayılar atan ve daha sonra dizinin elemanlarını, pozitif sayı adetini,negatif sayı adetini ve işaretsiz sayı adetini ekranda gösteriniz.
+            int pozitif = 0;
+            int negatif = 0;
+            int notr = 0;
+            Random rnd = new Random();
+            int[]sayilar=new int[20];
+            for (int i = 0; i < 20; i++)
+            {
+                sayilar[i] = rnd.Next(-100, 100);
+            }
+            foreach (int sayi in sayilar)
+            {
+             if (sayi > pozitif)
+                {
+                    pozitif++;
+                }
+             else if (sayi < negatif)
+                {
+                    negatif++;  
+                }
+             else
+                {
+                    notr++;
+                }
+                Console.WriteLine("pozitif sayı adeti"+pozitif);
+                Console.WriteLine("negatif sayı adeti"+negatif);
+                Console.WriteLine("nötr sayı adeti"+notr);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
